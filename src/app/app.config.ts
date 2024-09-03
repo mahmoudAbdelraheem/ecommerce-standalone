8e8ec11299ef9a0d3e8b38a581be25c782d014aa
mahmoudAbdelraheem <mmoud2031@gmail.com>
2024-09-03 13:47:01 +0200

    Revert "'worker'"

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,8 @@
-import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -11,15 +10,5 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     //? to use http client 
     provideHttpClient(withFetch()),
-    provideServiceWorker('ngsw-worker.js', {
-        enabled: !isDevMode(),
-        registrationStrategy: 'registerWhenStable:30000'
-    }), provideServiceWorker('ngsw-worker.js', {
-            enabled: !isDevMode(),
-            registrationStrategy: 'registerWhenStable:30000'
-          }), provideServiceWorker('ngsw-worker.js', {
-            enabled: !isDevMode(),
-            registrationStrategy: 'registerWhenStable:30000'
-          })
-],
+    ],
 };
